919033a7682d081673a52ac64a8e490d12baa513
HyunjiLee <hyunji.lee.tech@gmail.com>
2021-02-01 19:27:09 -0500

    Added selection pages import

--- App.js
+++ App.js
@@ -3,9 +3,16 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import WelcomeScreen from "./src/screens/WelcomeScreen";
 import HomeScreen from "./src/screens/HomeScreen";
+import DogSizeSelectionScreen from "./src/screens/DogTypeSelections/DogSizeSelectionScreen";
+import DogEnergyLevelSelectionScreen from "./src/screens/DogTypeSelections/DogEnergyLevelSelectionScreen";
+import DogColorSelectionScreen from "./src/screens/DogTypeSelections/DogColorSelectionScreen";
+
 export default function App() {
   return (
-    <WelcomeScreen />
+    // <WelcomeScreen />
     //<HomeScreen />
+    // <DogSizeSelectionScreen />
+    // <DogEnergyLevelSelectionScreen />
+    <DogColorSelectionScreen />
   );
 }
