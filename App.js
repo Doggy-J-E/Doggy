48469036940a6867144f8f9c83860306c8d81025
Elisa <elisangli11@gmail.com>
2021-03-15 16:41:21 -0400

    Fix issue #4 delete drawer and update tab

--- App.js
+++ App.js
@@ -1,29 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 
-import HomeStack from "./src/routes/HomeStack";
-import ProfileStack from "./src/routes/ProfileStack";
-
-const Tabs = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
-
-const TabsScreen = () => (
-  <Tabs.Navigator>
-    <Tabs.Screen name="Home" component={HomeStack} />
-    <Tabs.Screen name="Search" component={ProfileStack} />
-  </Tabs.Navigator>
-);
+import Tab from "./src/routes/TabsNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={TabsScreen} />
-        <Drawer.Screen name="About Us" component={ProfileStack} />
-      </Drawer.Navigator>
+      <Tab />
     </NavigationContainer>
   );
 }
