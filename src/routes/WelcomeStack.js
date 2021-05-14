d3c14100b477c9369fbfbb1d7b40e0bb287b6ca2
Elisa <elisangli11@gmail.com>
2021-05-13 21:45:01 -0400

    Fix #45 Update top navigation styling

--- src/routes/WelcomeStack.js
+++ src/routes/WelcomeStack.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { scale, moderateScale, verticalScale } from "../config/scaling";
 
 import HomeScreen from "../screens/HomeScreen";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import DetailsScreen from "../screens/DetailsScreen";
 import colors from "../config/colors";
+
+const MyCustomHeader = () => (
+  <Image
+    source={require("../../assets/doggy_logo_grey.png")}
+    style={{
+      width: scale(30),
+      height: verticalScale(30),
+      marginRight: scale(119),
+      marginTop: verticalScale(10),
+    }}
+  />
+);
 
 const Stack = createStackNavigator();
 
@@ -14,9 +28,15 @@ const WelcomeStack = () => {
       screenOptions={{
         headerStyle: {
           backgroundColor: colors.lightOrange,
+          elevation: 0,
+          shadowOpacity: 0,
+          borderBottomWidth: 0,
         },
         headerTintColor: colors.white,
         headerTitleAlign: "center",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
       }}
     >
       <Stack.Screen
@@ -27,12 +47,16 @@ const WelcomeStack = () => {
       <Stack.Screen
         name="HomeScreen"
         component={HomeScreen}
-        options={{ title: "Home" }}
+        options={{
+          title: "Doggy",
+          headerLeft: null,
+          headerRight: MyCustomHeader,
+        }}
       />
       <Stack.Screen
         name="DetailsScreen"
         component={DetailsScreen}
-        options={{ title: "DetailsScreen" }}
+        options={{ title: "Doggy Details" }}
       />
     </Stack.Navigator>
   );
